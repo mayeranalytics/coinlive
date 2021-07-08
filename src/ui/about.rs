c738eb64d48c3ffee8f341f7a73aff7332ade507
MM <mmayer@mayeranalytics.com>
2021-07-08 11:59:37 +0200

    warnings removed, about page nicer

--- src/ui/about.rs
+++ src/ui/about.rs
@@ -2,30 +2,39 @@
 use tui::{
     style::{Style, Color, Modifier},
     widgets::{Block, Borders, Paragraph, BorderType},
-    layout::{Layout, Constraint, Direction, Alignment, Rect},
+    layout::{Alignment},
     text::{Span, Spans},
 };
 use crate::version::VERSION;
-
+  
 pub fn about<'a>() -> Paragraph<'a> {
-    let txt = format!(
-        "\n\
-        coinlive - Live cryptocurrency prices in the CLI
-        \n\n\
-        (c) Mayer Analytics, GPL-2.0
-        \n\
-        Source: https://github.com/mayeranalytics/coinlive
-        \n\n\
-        Version: {}",
-        VERSION
-    );
+    let txt = vec![
+        Spans::from(Span::styled("     ####    #####    ######  ##   ##  ####      ######  ##   ##  #######  ", Style::default().fg(Color::LightCyan))),
+        Spans::from(Span::styled("    ##  ##  ### ###     ##    ###  ##   ##         ##    ##   ##   ##   #  ", Style::default().fg(Color::LightCyan))),
+        Spans::from(Span::styled("   ##       ##   ##     ##    #### ##   ##         ##    ##   ##   ##      ", Style::default().fg(Color::LightCyan))),
+        Spans::from(Span::styled("   ##       ##   ##     ##    #######   ##         ##     ## ##    ####    ", Style::default().fg(Color::LightCyan))),
+        Spans::from(Span::styled("   ##       ##   ##     ##    ## ####   ##         ##     ## ##    ##      ", Style::default().fg(Color::LightCyan))),
+        Spans::from(Span::styled("    ##  ##  ### ###     ##    ##  ###   ##  ##     ##      ###     ##   #  ", Style::default().fg(Color::LightCyan))),
+        Spans::from(Span::styled("     ####    #####    ######  ##   ##  #######   ######    ###    #######  ", Style::default().fg(Color::LightCyan))),
+        Spans::from(Vec::new()),
+        Spans::from(Vec::new()),
+        Spans::from(Span::styled("Live cryptocurrency prices in the CLI",
+                                 Style::default().fg(Color::LightCyan).add_modifier(Modifier::ITALIC))),
+        Spans::from(Vec::new()),
+        Spans::from(Vec::new()),
+        Spans::from(Span::styled("(c) Mayer Analytics, GPL-3.0", Style::default().fg(Color::Red))),
+        Spans::from(Vec::new()),
+        Spans::from(Span::styled("https://github.com/mayeranalytics/coinlive", Style::default().fg(Color::Green))),
+        Spans::from(Vec::new()),
+        Spans::from(Span::styled(format!("Version {}", VERSION), Style::default().fg(Color::Gray))),
+    ];
     Paragraph::new(txt)
         .style(Style::default().fg(Color::LightCyan))
         .alignment(Alignment::Center)
         .block(
             Block::default()
                 .borders(Borders::ALL)
-                .style(Style::default().fg(Color::White))
+                .style(Style::default().fg(Color::Gray).bg(Color::Black))
                 .title("About")
                 .border_type(BorderType::Plain),
         )
--- src/version.rs
+++ src/version.rs
@@ -1,3 +1,3 @@
 //! Just the version string
 
-pub const VERSION: &str = "0.2.0";
+pub const VERSION: &str = "0.2.1";
